4998d93b32317a9ce3927cad00fdace9d8d29e6b
Shahin058 <koku2013@mail.ru>
2024-08-29 18:45:30 +0400

    Versiya1

---
+++ main.js
@@ -0,0 +1,171 @@
+
+
+
+// let firstanme = "Sahin "
+// let lastname = "Ibrahimov."
+// let metn = "Tanisligimiza cox sadam"
+// console.log(firstanme+lastname+metn);
+
+
+
+
+// let fullname = prompt ("Sahin Ibrahimov") 
+// let a = fullname.split(' ')
+// let b = a[0]
+// let d = a[1]
+// let c = b[0]
+// console.log(c,d[0])
+
+
+
+// let ad = prompt ('Soyadinizi ve adiniz qeyd edin')
+// let b = ad.split(' ')
+// let a = b[0]
+// let c = b[1]
+// let d = a[0]
+// console.log(d,c[0])
+
+
+// let name = prompt ('Ad')
+// console.log(`Salam, ${name}`);
+
+
+
+
+// let eded = +prompt ('eded daxil edin')
+// let reqem = +prompt ('eded daxil edin')
+// console.log(eded * reqem);
+// console.log(eded - reqem);
+// console.log(eded / reqem);
+// console.log(eded+reqem);
+
+
+
+
+// let a = 12345
+// let b = 98765
+// let c = a*b
+// let d = String(c)
+// console.log(c,d[6]);
+
+
+
+
+
+// let name = prompt('Adiniz ve Soyadiniz')
+// let a = name.split(' ')
+// let b = a[0]
+// console.log(`Salam, ${b}`);
+
+
+// let qiymet = prompt('530.9 qiymeti daxil edin')
+// let a = qiymet.split('.')
+// console.log(`${a[0]} manat ${a[1]} qepik`)
+
+
+// let name = prompt('soyadiniz adiniz ata adiniz')
+// let a = name.split(' ')
+// let b = a[0]
+// let c = a[1]
+// let d = a[2]
+// console.log(b,`${c[0]}.${d[0]}`
+
+
+
+
+
+
+
+// let value = 10
+
+// console.log(value == 10);
+// console.log(value < 15);
+// console.log(value <= 11);
+// console.log(value === 10);
+// console.log(value != 20);
+
+
+// let age = prompt('yasinizi qeyd edin')
+// if(age > 18){
+//    alert('Acces grantied')
+// }
+// if(age < 18){
+//     alert('Acces denied')
+// }
+
+
+
+// let names = ['ali','zaur','fuad','kamran']
+
+// let a = names[0][0]
+// let b = names[1][0]
+// let c = names[2][0]
+// let d = names [3][0]
+
+
+// console.log(a,b,c,d);
+
+
+// let arr = ['Sahin']
+
+// for(let i = 0; i < arr.length; i++){
+
+//     console.log(arr[0].length);
+// }
+
+
+// let arr = ['Salam,','Men,','eli.']
+// let a = arr[0]
+// let b = arr[1]
+// let c = arr[2]
+
+// console.log(a+b+c);
+
+
+
+
+
+// let stockltem = {
+//     id: 10 ,
+//     place: 'vitrin',
+//     weight: 3,
+// }
+// delete stockltem.price
+// let shopltem = {...stockltem,price:3,name: 'armud'}
+// console.log(shopltem);
+
+
+
+// let a = document.querySelector('h1')
+// a.innerHTML = "GeForce 20 Series"
+// let b = document.querySelector(".primary")
+// b.style.color = '#ebebeb'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
